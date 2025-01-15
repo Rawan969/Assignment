5fac0ea66dda8d733680d1b736d87b1dcbecc0cd
Rawan Ibraheem <121896627+Rawan969@users.noreply.github.com>
2025-01-15 17:31:28 +0200

    Update PortfolioApplication.java

--- src/main/java/com/example/portfolio/PortfolioApplication.java
+++ src/main/java/com/example/portfolio/PortfolioApplication.java
@@ -18,7 +18,7 @@ public class PortfolioApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-		    .allowedOrigins("http://localhost:3000","https://assignment-frontend-lime.vercel.app")
+		    .allowedOrigins("http://localhost:3000","https://assignment-frontend-tau-beryl.vercel.app")
                     //.allowedOrigins("http://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
